0c357d4b16be41e4847a5d41339b38415dbc4583
Sanjeev kumar <sanjeevkrpd11@gmail.com>
2024-09-30 10:01:22 +0530

    codes

---
+++ TcsProlems/ReverseString.java
@@ -0,0 +1,21 @@
+package TcsProlems;
+
+import java.util.Stack;
+
+public class ReverseString {
+
+     public static void main(String[] args) {
+        String str = "Hello World";
+
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            
+            s.push(str.charAt(i));
+        }
+
+        while (!s.isEmpty()) {
+            System.out.print(s.pop());
+        }
+     }
+}
