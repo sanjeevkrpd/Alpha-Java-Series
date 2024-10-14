06d2c9666f45569e8262f8203cb62e5ee3242bb6
Sanjeev kumar <sanjeevkrpd11@gmail.com>
2024-10-14 17:45:04 +0530

    some changes

--- DP/CountSteps.java
+++ DP/CountSteps.java
@@ -2,6 +2,9 @@ package DP;
 import java.util.*;
 public class CountSteps {
 
+
+    // memoisation
+
         public static int  countWays(int n , int ways[]){
 
             if (n == 0) {
@@ -20,12 +23,31 @@ public class CountSteps {
             return ways[n];
         }
 
+        // tabulation
+
+      
+        public static int countWaysTab(int n) {
+
+            int dp[] = new int[n+1];
+            dp[0]=1;
+            dp[1]=1;
+
+            for (int i = 2; i < dp.length; i++) {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+
+            return dp[n];
+        }
+
     public static void main(String[] args) {
         
 
         
         int ways[] = new int[8+1];
-        Arrays.fill(ways , -1);
-        System.out.println(countWays(5 , ways));
+        // Arrays.fill(ways , -1);
+
+        // System.out.println(countWays(5 , ways));
+
+        System.out.println(countWaysTab(5));
     }
 }
